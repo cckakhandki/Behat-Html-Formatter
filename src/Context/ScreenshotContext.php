8ccd5dc764f1b0d8c58f3f9c73b50375f65f4bf2
Chetan Kakhandki <cckakhandki@gmail.com>
2016-05-01 22:00:24 +0530

    Improved Screenshot formatter, added ability tp print web page's url on the screenshot

--- src/Context/ScreenshotContext.php
+++ src/Context/ScreenshotContext.php
@@ -10,49 +10,67 @@ use Behat\Behat\Hook\Scope\BeforeScenarioScope;
  * Class BehatFormatterContext
  */
 class ScreenshotContext implements SnippetAcceptingContext {
-    private $currentScenario;
-    protected static $currentSuite;
     private $output_path;
-    private $screenshotName;
+    private $print_url;
     
-    /** 
-     * @var \Drupal\DrupalExtension\Context\MinkContext 
-     */
+    /** @var string $text_color Hex color code */
+    private $text_color;
+    
+    /** @var int $x X-coordinate */
+    private $x;
+    
+    /** @var int $x Y-coordinate */
+    private $y;
+    
+    /** @var \Drupal\DrupalExtension\Context\MinkContext */
     private $minkContext;
     
     /**
      * @param string $screenshot_path
      */
-    public function __construct($screenshot_path) {
-    	$this->output_path = $screenshot_path;
+    public function __construct($screenshot_path, $print_url = 'no', $text_color = '#ffffff', $x=50, $y=25) {
+        $this->output_path = $screenshot_path;
+        $this->print_url = $print_url;
+        $this->text_color = $text_color;
+        $this->x = $x;
+        $this->y = $y;
+        var_dump($x . PHP_EOL . $y);
     }
     
-	/** @BeforeScenario */
- 	public function gatherContexts(BeforeScenarioScope $scope)
- 	{
- 		$environment = $scope->getEnvironment();
-	
- 		$this->minkContext = $environment-> getContext('Drupal\DrupalExtension\Context\MinkContext');
- 	}
+    /**
+     * @BeforeScenario
+     */
+    public function gatherContexts(BeforeScenarioScope $scope) {
+        $environment = $scope->getEnvironment();
+        $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
+    }
     
     /**
      * @Then /^I take screenshot of current page$/
      */
-    public function getScreenshot($return = FALSE)
-    {
-    	if (!is_dir($this->output_path)){
-    		mkdir($this->output_path, 0755, TRUE);
-    	}
-    	$url = $this->minkContext->getSession()->getCurrentUrl();
-    	$browser = $this->minkContext->getMinkParameter('browser_name');
-    	$fileName = $browser . '-' . date('Y-m-d-H-i-s') . '.png';
-    
-    	$this->minkContext->saveScreenshot($fileName, $this->output_path);
-    	$path = $this->output_path.'/'.$fileName;
-    	print "screenshot taken at : " . $path;
-    	
-    	if($return)
-    		return $fileName;
+    public function getScreenshot($return = FALSE) {
+        if (!is_dir($this->output_path)) {
+            mkdir ($this->output_path, 0755, TRUE);
+        }
+        $browser = $this->minkContext->getMinkParameter('browser_name');
+        $fileName = $browser . '-' . date('Y-m-d-H-i-s') . '.png';
+        $this->minkContext->saveScreenshot($fileName, $this->output_path);
+        $screenshot_path = $this->output_path . '/' . $fileName;
+        print "Screenshot saved at : " . realpath($screenshot_path);
+        if (strcasecmp($this->print_url, 'yes') == 0) {
+            $url = $this->minkContext->getSession()->getCurrentUrl();
+            $image = imagecreatefrompng(realpath($screenshot_path));
+            list($r, $g, $b) = sscanf($this->text_color, "#%02x%02x%02x");
+            $color = imagecolorallocate($image, $r, $g, $b);
+            // Write page url on the screenshot.
+            if (imagestring($image, 5, $this->x, $this->y, $url, $color)) {
+                // Save the file.
+                imagepng($image, realpath($screenshot_path));
+            }
+            // Clear Memory
+            imagedestroy ($image);
+        }
+        if ($return)
+            return $fileName;
     }
-    
 }
